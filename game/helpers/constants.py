01427bd454ad1011fda6aaa2d1c7cb9efac6ac68
emcxcme <54064481+emcxcme@users.noreply.github.com>
2020-04-02 10:17:21 +0000

    Add files via upload

---
+++ game/helpers/constants.py
@@ -0,0 +1,20 @@
+
+class Constants:
+
+    SLITHERIN_NAME = "Slitherin"
+    ICON_PATH = "./assets/slitherin_icon.png"
+    FONT = "Arial"
+    MODEL_FEATURE_COUNT = 5
+    MODEL_NAME = "model.tf"
+    DQN_MODEL_NAME = "model.h5"
+    CHECKPOINT_NAME = "model.ckpt"
+    MODEL_DIRECTORY = "./tf_models/"
+    NAVIGATION_BAR_HEIGHT = 30
+    FPS = 60
+    PIXEL_SIZE = 25
+    SCREEN_WIDTH = 500
+    SCREEN_HEIGHT = 500
+    FRAMES_TO_REMEMBER = 4
+    SCREEN_DEPTH = 32
+    ENV_HEIGHT = SCREEN_HEIGHT/PIXEL_SIZE
+    ENV_WIDTH = SCREEN_WIDTH/PIXEL_SIZE
